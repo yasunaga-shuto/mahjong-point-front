2fcf363aaf42dd5b986dfb43e358cf6f59952a09
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-16 10:16:13 +0900

    修正

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
     <OrganismsHeader />
+    <div class="container mx-auto w-3/4 my-8">
+      <div class="flex items-center gap-6 mb-6">
+        <!-- 場風 -->
+        <div class="flex items-center">
+          <select id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="east" selected>東</option>
+            <option value="south">南</option>
+            <option value="west">西</option>
+            <option value="north">北</option>
+          </select>
+          <label class="ml-2">場</label>
+        </div>
+        <!-- 本場 -->
+        <div class="flex items-center">
+          <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="w-16 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+
+          <label class="w-full ml-2">本場</label>
+        </div>
+        <!-- 自風 -->
+        <div class="flex items-center">
+          <select id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="east" selected>東</option>
+            <option value="south">南</option>
+            <option value="west">西</option>
+            <option value="north">北</option>
+          </select>
+          <label class="ml-2">家</label>
+        </div>
+      </div>
+      <!-- 手牌 -->
+      <div class="border w-full h-36 border-gray-400 rounded-md">
+      </div>
+    </div>
   </div>
 </template>
