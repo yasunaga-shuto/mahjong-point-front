94f15bce5e6e9cc4dcc2d057f21f4081d988e294
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-16 12:36:18 +0900

    修正

--- app.vue
+++ app.vue
@@ -31,7 +31,8 @@
         </div>
       </div>
       <!-- 手牌 -->
-      <div class="border w-full h-36 border-gray-400 rounded-md mb-6">
+      <div class="border w-full h-36 border-gray-400 rounded-md mb-6 flex items-center justify-center">
+        <img v-for="t in tehai" :key="t" :src="`/pai/normal/${t}.gif`" class="w-12 h-14">
       </div>
       <!-- モード -->
       <div class="mb-6">
@@ -46,22 +47,22 @@
       <div class="flex">
         <div class="h-36 w-3/6">
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/p_ms${i}_1.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
+            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}m.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}m`)">
           </div>
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/p_ps${i}_1.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
+            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}p.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}p`)">
           </div>
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/p_ss${i}_1.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
+            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}s.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}s`)">
           </div>
           <div class="mb-3">
-            <img src="/pai/p_ji_e_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_ji_s_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_ji_w_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_ji_n_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_no_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_ji_h_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
-            <img src="/pai/p_ji_c_1.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3">
+            <img src="/pai/normal/ton.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('ton')">
+            <img src="/pai/normal/nan.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('nan')">
+            <img src="/pai/normal/sha.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('sha')">
+            <img src="/pai/normal/pei.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('pei')">
+            <img src="/pai/normal/haku.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('haku')">
+            <img src="/pai/normal/hatsu.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('hatsu')">
+            <img src="/pai/normal/chun.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('chun')">
           </div>
         </div>
         <div class="w-3/6">
@@ -128,6 +129,11 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
+type Manzu = '1m' | '2m' | '3m' | '4m' | '5m' | '6m' | '7m' | '8m' | '9m'
+type Pinzu = '1p' | '2p' | '3p' | '4p' | '5p' | '6p' | '7p' | '8p' | '9p'
+type Sozu = '1s' | '2s' | '3s' | '4s' | '5s' | '6s' | '7s' | '8s' | '9s'
+type Tupai = 'ton' | 'nan' | 'sha' | 'pei' | 'haku' | 'hatsu' | 'chun'
+
 const mode = ref('')
 const how = ref('')
 const riichi = ref('なし')
@@ -136,4 +142,13 @@ const chankan = ref(false)
 const linshan = ref(false)
 const haitei = ref(false)
 const hora = ref(false)
+
+const tehai = ref<(Manzu | Pinzu | Sozu | Tupai)[]>([])
+
+const addPai = (pai: Manzu | Pinzu | Sozu | Tupai) => {
+  if (tehai && tehai.value.length >= 14) {
+    return
+  }
+  tehai.value.push(pai)
+}
 </script>
