56f750da3088e5a670e9c9fcb297dd602f5bd0de
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-16 14:38:28 +0900

    chi追加

--- app.vue
+++ app.vue
@@ -5,27 +5,27 @@
       <div class="flex items-center gap-6 mb-6">
         <!-- 場風 -->
         <div class="flex items-center">
-          <select id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-            <option value="east" selected>東</option>
-            <option value="south">南</option>
-            <option value="west">西</option>
-            <option value="north">北</option>
+          <select v-model="ba" id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="ton" selected>東</option>
+            <option value="nan">南</option>
+            <option value="sha">西</option>
+            <option value="pei">北</option>
           </select>
           <label class="ml-2">場</label>
         </div>
         <!-- 本場 -->
         <div class="flex items-center">
-          <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="w-16 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" required />
+          <input v-model="honba" type="number" id="number-input" aria-describedby="helper-text-explanation" class="w-16 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" required />
 
           <label class="w-full ml-2">本場</label>
         </div>
         <!-- 自風 -->
         <div class="flex items-center">
-          <select id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-            <option value="east" selected>東</option>
-            <option value="south">南</option>
-            <option value="west">西</option>
-            <option value="north">北</option>
+          <select v-model="jicha" id="countries" class="w-16 border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="ton" selected>東</option>
+            <option value="nan">南</option>
+            <option value="sha">西</option>
+            <option value="pei">北</option>
           </select>
           <label class="ml-2">家</label>
         </div>
@@ -33,37 +33,43 @@
       <!-- 手牌 -->
       <div class="border w-full h-36 border-gray-400 rounded-md mb-6 flex items-center justify-center relative">
         <el-button class="absolute top-4 right-8" @click="sort">並び替え（埋牌）</el-button>
-        <img v-for="(t, index) in tehai" :key="index" :src="`/pai/normal/${t}.gif`" class="w-12 h-14">
+        <img v-for="(t, index) in tehai" :key="index" :src="`/pai/${t}.gif`" class="w-12 h-14">
+
+        <div v-for="(t, i) in hupai" :key="`hupai-${i}`" class="ml-4">
+          <span v-for="(p, j) in t.pai" :key="`hupai-${i}-${j}`">
+            <img :src="`/pai/${p}.gif`" class="w-12 h-14 inline-block" :class="{ 'rotate-[270deg]': j === 0 }">
+          </span>
+        </div>
       </div>
       <!-- モード -->
       <div class="mb-6">
-        <el-radio-group v-model="mode" size="large">
-          <el-radio-button label="チー" value="チー" />
-          <el-radio-button label="ポン" value="ポン" />
-          <el-radio-button label="カン" value="カン" />
-          <el-radio-button label="暗カン" value="暗カン" />
-          <el-radio-button label="ドラ" value="ドラ" />
+        <el-radio-group v-model="mode" size="large" @input="inputMode">
+          <el-radio-button label="チー" value="chi" />
+          <el-radio-button label="ポン" value="pon" />
+          <el-radio-button label="カン" value="kan" />
+          <el-radio-button label="暗カン" value="ankan" />
+          <el-radio-button label="ドラ" value="dora" />
         </el-radio-group>
       </div>
       <div class="flex">
         <div class="h-36 w-3/6">
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}m.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}m`)">
+            <img v-for="i in 9" :key="i" :src="`/pai/${i}m.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}m`)">
           </div>
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}p.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}p`)">
+            <img v-for="i in 9" :key="i" :src="`/pai/${i}p.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}p`)">
           </div>
           <div class="mb-3">
-            <img v-for="i in 9" :key="i" :src="`/pai/normal/${i}s.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}s`)">
+            <img v-for="i in 9" :key="i" :src="`/pai/${i}s.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai(`${i}s`)">
           </div>
           <div class="mb-3">
-            <img src="/pai/normal/ton.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('ton')">
-            <img src="/pai/normal/nan.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('nan')">
-            <img src="/pai/normal/sha.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('sha')">
-            <img src="/pai/normal/pei.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('pei')">
-            <img src="/pai/normal/haku.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('haku')">
-            <img src="/pai/normal/hatsu.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('hatsu')">
-            <img src="/pai/normal/chun.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('chun')">
+            <img src="/pai/ton.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('ton')">
+            <img src="/pai/nan.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('nan')">
+            <img src="/pai/sha.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('sha')">
+            <img src="/pai/pei.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('pei')">
+            <img src="/pai/haku.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('haku')">
+            <img src="/pai/hatsu.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('hatsu')">
+            <img src="/pai/chun.gif" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" @click="addPai('chun')">
           </div>
         </div>
         <div class="w-3/6">
@@ -140,8 +146,13 @@ type Pinzu = typeof PINZU[number]
 type Sozu = typeof SOZU[number]
 type Tupai = typeof TUPAI[number]
 type Pai = Manzu | Pinzu | Sozu | Tupai
+type Mode = 'chi' | 'pon' | 'kan' | 'ankan' | 'dora' | ''
 
-const mode = ref('')
+const ba = ref('ton')
+const honba = ref(0)
+const jicha = ref('ton')
+
+const mode = ref<Mode>('')
 const how = ref('')
 const riichi = ref('なし')
 const ippatsu = ref(false)
@@ -151,12 +162,27 @@ const haitei = ref(false)
 const hora = ref(false)
 
 const tehai = ref<Pai[]>([])
+const hupai = ref<{ type: Mode, pai: Pai[] }[]>([])
 
 const addPai = (pai: Pai) => {
-  if (tehai && tehai.value.length >= 14) {
+  if (tehai.value.length + hupai.value.length * 3 >= 14) {
     return
   }
-  tehai.value.push(pai)
+  switch (mode.value) {
+  case 'chi':
+    const last = hupai.value.slice(-1)[0]
+    last.pai.push(pai)
+    hupai.value[hupai.length - 1] = last
+    if (last && last.pai.length === 3) {
+      mode.value = ''
+    }
+    break
+  default:
+    tehai.value.push(pai)
+  }
+}
+const inputMode = (event: { target: HTMLInputElement }) => {
+  hupai.value.push({ type: event.target.value, pai: [] })
 }
 const sort = () => {
   const order = [...MANZU, ...PINZU, ...SOZU, ...TUPAI]
