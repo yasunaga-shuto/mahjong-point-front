de6b3923abdabb69ff43ef26cf5c391395553858
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-23 11:37:37 +0900

    筒子入れ替え

--- app.vue
+++ app.vue
@@ -58,7 +58,7 @@
             <img v-for="(man, index) in MANZU" :key="index" :src="`/pai/${man}.png`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" width="47" @click="addPai(man)">
           </div>
           <div class="mb-3">
-            <img v-for="(pin, index) in PINZU" :key="index" :src="`/pai/${pin}.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" width="47" @click="addPai(pin)">
+            <img v-for="(pin, index) in PINZU" :key="index" :src="`/pai/${pin}.png`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" width="47" @click="addPai(pin)">
           </div>
           <div class="mb-3">
             <img v-for="(sou, index) in SOZU" :key="index" :src="`/pai/${sou}.gif`" alt="pai1" class="inline-block cursor-pointer hover:-mt-3" width="47" @click="addPai(sou)">
