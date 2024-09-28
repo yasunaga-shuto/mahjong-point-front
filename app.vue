1b715f12a5faefd9823e739e32d9ae6bb8718d1d
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-09-28 15:16:23 +0900

    場風追加

--- app.vue
+++ app.vue
@@ -407,6 +407,7 @@ const calculate = async () => {
       melds: hupai.value,
       has_aka_dora: hasAkaDora.value,
       is_riichi: riichi.value === 'リーチ',
+      round_wind: roundWind.value,
       player_wind: playerWind.value,
     }
   })
