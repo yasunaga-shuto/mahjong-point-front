2aa6c05e5799aed7424644678ba1ddbdfca84e89
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-10-27 15:58:37 +0900

    ogp設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja', prefix: 'og: https://ogp.me/ns#'
+      },
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: '麻雀点数計算' },
+        { property: 'og:image', content: 'https://mahjong-point-front.vercel.app/ogp.jpg', },
+      ]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
