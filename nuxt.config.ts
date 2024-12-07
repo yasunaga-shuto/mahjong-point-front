4857f4445a83f852570d58ae8024728eae79a1ca
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-12-07 19:07:25 +0900

    メタタグ追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,13 +7,23 @@ export default defineNuxtConfig({
       },
       meta: [
         { property: 'og:type', content: 'website' },
-        { property: 'og:site_name', content: '麻雀点数計算' },
+        { property: 'og:site_name', content: '自動麻雀点数計算アプリ' },
         { property: 'og:image', content: 'https://mahjong-point-front.vercel.app/ogp.jpg', },
         { hid: 'og-url', property: 'og:url', content: 'https://mahjong-point-front.vercel.app' },
         {
+          hid: 'description',
+          name: 'description',
+          content: '麻雀の点数計算を初心者の方にも優しく簡単にできるサイトです。初心者の方や点数計算が苦手な方は是非ご活用ください。'
+        },
+        {
+          hid: 'description',
+          name: 'keywords',
+          content: '麻雀,ドラ表示牌,点数,点数計算,自動,麻雀点数計算,麻雀牌'
+        },
+        {
           hid: 'og-description',
           property: 'og:description',
-          content: '麻雀の点数計算をシンプルにできるサイトです。初心者の方や点数計算が苦手な方、是非ご活用ください。',
+          content: '麻雀の点数計算を初心者の方にも優しく簡単にできるサイトです。初心者の方や点数計算が苦手な方は是非ご活用ください。',
         },
         {
           hid: "twitter-card",
