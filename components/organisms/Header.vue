c9547ef5f5286a2375a017fe20da2b566e2bb2ce
yasunaga shuto <yasunaga.shuto@gmail.com>
2024-12-07 19:09:42 +0900

    h1に変更

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -3,7 +3,7 @@
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
-          <span class="text-xl">麻雀点数計算</span>
+          <h1 class="text-xl">麻雀点数計算</h1>
         </a>
       </div>
       <div class="flex lg:hidden">
